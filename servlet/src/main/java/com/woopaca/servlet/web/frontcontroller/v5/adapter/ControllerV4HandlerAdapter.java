75e6dc585abf521b0f58bcd20ef0a35706db56f8
woopaca <jcw001031@gmail.com>
2023-01-29 15:06:22 +0900

    어댑터 패턴 사용으로 확장 편의성 증가

---
+++ servlet/src/main/java/com/woopaca/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -0,0 +1,45 @@
+package com.woopaca.servlet.web.frontcontroller.v5.adapter;
+
+import com.woopaca.servlet.web.frontcontroller.ModelView;
+import com.woopaca.servlet.web.frontcontroller.v4.ControllerV4;
+import com.woopaca.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean isSupport(Object handler) {
+        return handler instanceof ControllerV4;
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV4 controller = (ControllerV4) handler;
+
+        Map<String, String> paramMap = createParamMap(request);
+        Map<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+        ModelView modelView = new ModelView(viewName);
+
+        Map<String, Object> modelViewModel = modelView.getModel();
+        modelViewModel.putAll(model);
+
+        return modelView;
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator().forEachRemaining(paramName -> {
+            paramMap.put(paramName, request.getParameter(paramName));
+        });
+
+        return paramMap;
+    }
+}
