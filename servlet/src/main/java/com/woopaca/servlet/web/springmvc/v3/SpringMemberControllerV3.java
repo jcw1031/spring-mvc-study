ea27bfdd8ef7081b8bd09d90dd516d2b39625f25
woopaca <jcw001031@gmail.com>
2023-02-01 14:08:56 +0900

    @GetMapping, @PostMapping 사용

--- servlet/src/main/java/com/woopaca/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ servlet/src/main/java/com/woopaca/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,8 +4,9 @@ import com.woopaca.servlet.domain.member.Member;
 import com.woopaca.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -16,12 +17,12 @@ public class SpringMemberControllerV3 {
 
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
     public String save(
             @RequestParam("username") String username,
             @RequestParam("age") int age,
@@ -33,7 +34,7 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @GetMapping("")
     public String members(Model model) {
         List<Member> members = memberRepository.findAll();
 
