fcb3ac11c925244e29f70661d9bd21c19757a1a5
woopaca <jcw001031@gmail.com>
2023-02-03 13:55:05 +0900

    Request Param을 가져오는 여러 방법

---
+++ springmvc/src/main/java/com/woopaca/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,80 @@
+package com.woopaca.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username = {}, age = {}", username, age);
+
+        response.getWriter().write("OK");
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String username,
+                                 @RequestParam("age") int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3") // 변수명이 같으면 @RequestParam만 사용해도 된다.
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4") // String, Integer, int 등 단순 타입이면 @RequestParam도 생략이 가능하다.
+    public String requestParamV4(String username, int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(@RequestParam(required = true) String username,
+                                       @RequestParam(required = false) Integer age) {
+        log.info("username = {}, age = {}", username, age);
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default") // defaultValue는 빈 문자도 defaultValue로 설정한다.
+    public String requestParamDefault(@RequestParam(required = true, defaultValue = "guest") String username,
+                                      @RequestParam(required = false, defaultValue = "-1") int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map") // Map으로 모든 파라미터 받아오기
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-multi-map") // MultiValueMap은 하나의 키에 값이 여러 개인 경우 사용
+    public String requestParamMultiValueMap(@RequestParam MultiValueMap<String, Object> paramMap) {
+        log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "OK";
+    }
+}
