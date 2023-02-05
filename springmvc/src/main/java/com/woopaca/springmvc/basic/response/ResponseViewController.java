e9691a88ac3b7db32acc971f17146988178fae1c
woopaca <jcw001031@gmail.com>
2023-02-05 11:34:23 +0900

    HTTP 응답 정적 리소스 및 뷰 템플릿

---
+++ springmvc/src/main/java/com/woopaca/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,30 @@
+package com.woopaca.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        return new ModelAndView("response/hello")
+                .addObject("data", "hello");
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+
+    /**
+     * 권장하는 방법은 아니다.
+     */
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
+}
