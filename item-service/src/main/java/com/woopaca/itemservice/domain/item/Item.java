c96d5958b81d1fe79332acc8fd6285c1ff9d9c62
woopaca <jcw001031@gmail.com>
2023-02-07 13:42:48 +0900

    Item 도메인 모델 추가

---
+++ item-service/src/main/java/com/woopaca/itemservice/domain/item/Item.java
@@ -0,0 +1,25 @@
+package com.woopaca.itemservice.domain.item;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@Getter
+@NoArgsConstructor
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer itemPrice;
+    private Integer quantity;
+
+    @Builder
+    public Item(Long id, String itemName, Integer itemPrice, Integer quantity) {
+        this.id = id;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.quantity = quantity;
+    }
+}
